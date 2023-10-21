6f072f3a6b317341c64715b3360ad62a56036251
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2023-10-21 11:17:38 -0600

    moved menu into its own function and fixed main

--- SQL_Injection_Mitigation.py
+++ SQL_Injection_Mitigation.py
@@ -1,7 +1,8 @@
 import sqlite3
+import os
 
 def testValid(function):
-    '''Runs through a set of test cases that represents valid input through the function passed as parameter.'''
+    '''Runs a set of test cases that represents valid input through the function passed as parameter.'''
 
     # Valid test cases [USERNAME, PASSWORD]
     valid_tests = [
@@ -12,15 +13,18 @@ def testValid(function):
         ["Alex_Calva_555","Al3x_Calva_123"]
     ]
 
+    print()
     print("Valid Input Test Cases:")
+    print()
     counter = 1
     for test in valid_tests:
         query = function(test[0],test[1])
-        print(f'{counter}. Username: {test[0]} Password: {test[1]} Resulting Query: {query}')
+        print(f'{counter}. USERNAME: {test[0]}    PASSWORD: {test[1]}')
+        print(f'   RESULTING QUERY: {query}\n')
         counter += 1
 
 def testTautology(function):
-    '''Runs through a set of test cases that represents tautology attacks through the function passed as parameter.'''
+    '''Runs a set of test cases that represents tautology attacks through the function passed as parameter.'''
 
     # Tautology test cases [USERNAME, PASSWORD]
     tautology_test = [
@@ -31,15 +35,18 @@ def testTautology(function):
         ["Alex_Calva_555","' OR 'x'='x"]
     ]
 
+    print()
     print("Tautology Attack Test Cases:")
+    print()
     counter = 1
     for test in tautology_test:
         query = function(test[0], test[1])
-        print(f'{counter}. Username: {test[0]} Password: {test[1]} Resulting Query: {query}')
+        print(f'{counter}. USERNAME: {test[0]}    PASSWORD: {test[1]}')
+        print(f'   RESULTING QUERY: {query}\n')
         counter += 1
 
 def testAddState(function):
-    '''Runs through a set of test cases that represents additional statements attacks through the function passed as a parameter.'''
+    '''Runs a set of test cases that represents additional statements attacks through the function passed as a parameter.'''
     
     # Additional statement cases [USERNAME, PASSWORD]
     add_statement_tests = [
@@ -50,15 +57,18 @@ def testAddState(function):
         ["Alex_Calva_555", "pass'; DELETE users WHERE username = 'DylanRuppell_42"]
     ]
 
+    print()
     print("Additional Statement Attack Test Cases:")
+    print()
     counter = 1
     for test in add_statement_tests:
         query = function(test[0], test[1])
-        print(f'{counter}. Username: {test[0]} Password: {test[1]} Resulting Query: {query}')
+        print(f'{counter}. USERNAME: {test[0]}    PASSWORD: {test[1]}') 
+        print(f'    RESULTING QUERY: {query}\n')
         counter += 1
 
 def testComment(function):
-    '''Runs through a set of test cases that represents comment attacks through the function passed as a parameter.'''
+    '''Runs a set of test cases that represents comment attacks through the function passed as a parameter.'''
 
     comment_attack_tests = [
         # The first 3 tests assume the username is known by the attacker
@@ -71,15 +81,18 @@ def testComment(function):
         ["Root'; -- ","thanksfortheaccess"]
     ]
 
-    print("Additional Statement Attack Test Cases:")
+    print()
+    print("Comment Attack Test Cases:")
+    print()
     counter = 1
     for test in comment_attack_tests:
         query = function(test[0], test[1])
-        print(f'{counter}. Username: {test[0]} Password: {test[1]} Resulting Query: {query}')
+        print(f'{counter}. USERNAME: {test[0]} PASSWORD: {test[1]}') 
+        print(f'    RESULTING QUERY: {query}\n')
         counter += 1
 
 def testUnion(function):
-    '''Runs through a set of test cases that represents union query attacks through the function passed as a parameter.'''
+    '''Runs a set of test cases that represents union query attacks through the function passed as a parameter.'''
 
     union_query_attack_tests = [
         # Pull a list of passwords
@@ -94,11 +107,14 @@ def testUnion(function):
         ["Alex_Calva_555","helloworld' UNION INSERT INTO users (username, password) VALUES ('HackerUsername', 'HackedYou428!')"]
     ]
 
-    print("Additional Statement Attack Test Cases:")
+    print()
+    print("Union Query Attack Test Cases:")
+    print()
     counter = 1
     for test in union_query_attack_tests:
         query = function(test[0], test[1])
-        print(f'{counter}. Username: {test[0]} Password: {test[1]} Resulting Query: {query}')
+        print(f'{counter}. USERNAME: {test[0]}     PASSWORD: {test[1]}')
+        print(f'RESULTING QUERY: {query}\n')
         counter += 1
 
 
@@ -161,60 +177,46 @@ def show_database():
     connection.close()
     return result
 
+def displayMenu():
+    os.system('cls')
+    print("SQL Injection Mitigation Lab")
+    print("1. Test Valid Input Test Cases")
+    print("2. Tautology Attack Test Cases")
+    print("3. Union Query Attack Test Cases")
+    print("4. Additional Statement Attack Test Cases")
+    print("5. Comment Attack Test Cases")
+    print("6. Weak Mitigation Function")
+    print("7. Strong Mitigation Function")
+    print("8. Exit")
+
 # Main function with menu NEED TO CHANGE, NO INPUT IS NEEDED FOR ASSIGNMENT
 def main():
-    print("SQL Injection Testing Program")
-    print("1. Test Valid Input")
-    print("2. Tautology Attack")
-    print("3. Union Query Attack")
-    print("4. Additional Statement Attack")
-    print("5. Comment Attack")
-    print("6. Weak Mitigation")
-    print("7. Strong Mitigation (SQLite)")
-    print("8. Show database users")
-    print("9. Exit")
+    displayMenu()
+    choice = input("Select an option: ")
 
-    while True:
-        choice = input("Select an option: ")
-        if choice == '1':
-            username = input("Enter a username: ")
-            password = input("Enter a password: ")
-            test_case(username, password)
-        elif choice == '2':
-            username = input("Enter a username: ")
-            password = input("Enter a password: ")
-            tautology_attack(username, password)
-        elif choice == '3':
-            username = input("Enter a username: ")
-            password = input("Enter a password: ")
-            union_query_attack(username, password)
-        elif choice == '4':
-            username = input("Enter a username: ")
-            password = input("Enter a password: ")
-            additional_statement_attack(username, password)
-        elif choice == '5':
-            username = input("Enter a username: ")
-            password = input("Enter a password: ")
-            comment_attack(username, password)
-        elif choice == '6':
-            input_str = input("Enter a string: ")
-            sanitized_input = weak_mitigation(input_str)
-            print(f"Sanitized Input: {sanitized_input}")
-        elif choice == '7':
-            username = input("Enter a username: ")
-            password = input("Enter a password: ")
-            result = strong_mitigation(username, password)
-            if result:
-                print("Authentication successful.")
-            else:
-                print("Authentication failed.")
-        elif choice == '8':
-            result = show_database()
-            print(result)
-        elif choice == '9':
-            break
-        else:
-            print("Invalid choice. Please select a valid option.")
+    while (choice != '8'):
+
+        if (choice == '1'):
+            testValid(genQuery)
+            print()
+        
+        elif (choice == '2'):
+            testTautology(genQuery)
+            print()
+        
+        elif (choice == '3'):
+            testUnion(genQuery)
+            print()
+
+        elif (choice == '4'):
+            testAddState(genQuery)
+            print()
+
+        elif (choice == '5'):
+            testComment(genQuery)
+            print()
+
+        choice = '8' # FOR TESTING PURPOSES
 
 if __name__ == "__main__":
     main()
